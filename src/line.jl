bbd7b075c2857519714c1902d04c683c5449bec8
Scott Lundberg <scottmlundberg@gmail.com>
2015-11-17 17:48:42 -0800

    Added line plot

--- src/line.jl
+++ src/line.jl
@@ -1,14 +1,12 @@
-using PyPlot
-
-function bar(;
+function line(;
         x=nothing, y=nothing, color=nothing,
         colors=["#3366CC", "#DC3912", "#FF9902", "#0C9618"],
-        legendLoc="upper right", figsize=(6, 4), xlabel=nothing, ylabel=nothing)
+    legendLoc="upper right", figsize=(6, 4), xlabel=nothing, ylabel=nothing)
     xvalues = unique(x)
     xcount = Int64[sum(x .== v) for v in xvalues]
 
     cvalues = unique(color)
-
+    
     fig,ax = subplots(figsize=figsize)
     ax[:spines]["right"][:set_visible](false)
     ax[:spines]["top"][:set_visible](false)
@@ -18,20 +16,12 @@ function bar(;
     width = 1/length(cvalues) * 0.8
     groupSpacing = 0.2
 
-    bars = Any[]
+    lines = Any[]
     for (i,c) in enumerate(cvalues)
-        vals = zeros(length(xvalues))
-        for j in 1:length(x)
-            if color[j] == c
-                vals[findfirst(xvalues, x[j])] = y[j]
-            end
-        end
-        push!(bars, ax[:bar](ind .+ (i-1)*width + groupSpacing, vals, width*0.8, color=colors[i], edgecolor="none"))
+        push!(lines, ax[:plot](x[color .== c], y[color .== c], color=colors[i], linewidth=2)[1])
     end
-    ax[:set_xticks](ind .+ width*maximum(xcount)/2 + groupSpacing - width*0.1)
-    ax[:set_xticklabels](("G1", "G2", "G3", "G4", "G5"))
     if legendLoc != "none"
-        ax[:legend](bars, cvalues, frameon=false)
+        ax[:legend](lines, cvalues, frameon=false)
     end
     if xlabel != nothing
         ax[:set_xlabel](xlabel)
---
+++ test/line.jl
@@ -0,0 +1,6 @@
+
+SimplePlot.line(
+    x = [1, 2, 3, 4, 1, 2, 3, 4],
+    y = [3,6,2,4,8,3,5,1],
+    color = ["InvCorr", "InvCorr", "InvCorr", "InvCorr", "Corr", "Corr", "Corr", "Corr"]
+)
