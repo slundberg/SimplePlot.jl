3e1caed2e9ae85c13acf89fbb2aa0bfd71c0f98c
Scott Lundberg <scottmlundberg@gmail.com>
2015-11-18 08:46:42 -0800

    Allow bars of varying colors for each x value

--- src/bar.jl
+++ src/bar.jl
@@ -13,18 +13,27 @@ function bar(;kwargs...)
     xcount = Int64[sum(x .== v) for v in xvalues]
     cvalues = unique(color)
     ind = 1:length(xvalues)
-    width = 1/length(cvalues) * 0.8
+    width = 1/maximum(xcount) * 0.8
     groupSpacing = 0.2
 
+    # find which colors are present at each x value
+    cgroups = Any[]
+    for i in 1:length(xvalues)
+        push!(cgroups, color[find(x .== xvalues[i])])
+    end
+    
     bars = Any[]
     for (i,c) in enumerate(cvalues)
-        vals = zeros(length(xvalues))
-        for j in 1:length(x)
-            if color[j] == c
-                vals[findfirst(xvalues, x[j])] = y[j]
+        vals = ones(length(xvalues))*Inf
+        vals = Float64[]
+        pos = Float64[]
+        for j in 1:length(xvalues)
+            cind = findfirst(cgroups[j] .== c)
+            if cind != 0
+                push!(pos, j + (cind-1)*width + groupSpacing)
+                push!(vals, y[findfirst((color .== c) .* (x .== xvalues[j]))])
             end
         end
-        pos = ind .+ (i-1)*width + groupSpacing
         push!(bars, ax[:bar](pos, vals, width*0.8, color=colors[i], edgecolor="none"))
     end
     ax[:set_xticks](ind .+ width*maximum(xcount)/2 + groupSpacing - width*0.1)
@@ -33,4 +42,5 @@ function bar(;kwargs...)
     loc = get(kwargs, :legend, "upper right")
     loc != "none" && ax[:legend](bars, cvalues, frameon=false, loc=loc)
     fig
+    1
 end
