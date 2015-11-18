a96e985bcbced20446f5fec24bdd820f24d50124
Scott Lundberg <scottmlundberg@gmail.com>
2015-11-18 10:21:36 -0800

    Fix labeling bug in line plot

--- src/line.jl
+++ src/line.jl
@@ -2,7 +2,7 @@ function line(;kwargs...)
     kwargs = Dict(kwargs)
     fig,ax = subplots(figsize=get(kwargs, :figsize, (5, 4)))
     x,y,color = set_defaults(ax; kwargs...)
-    
+
     colors = get(kwargs, :colors, defaultColors)
     cvalues = unique(color)
     lines = Any[]
@@ -12,6 +12,6 @@ function line(;kwargs...)
     end
 
     loc = get(kwargs, :legend, "upper right")
-    loc != "none" && ax[:legend](lines, cvalues, frameon=false, loc=loc)
+    loc != "none" && ax[:legend](lines, reverse(cvalues), frameon=false, loc=loc)
     fig
 end
