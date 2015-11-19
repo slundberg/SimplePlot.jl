97a52e252a2ee6842be00596649601edfbe53d2c
Scott Lundberg <scottmlundberg@gmail.com>
2015-11-19 13:37:17 -0800

    Allow stacked bars

--- src/bar.jl
+++ src/bar.jl
@@ -2,6 +2,7 @@
 function bar(;kwargs...)
     kwargs = Dict(kwargs)
     fig,ax = subplots(figsize=get(kwargs, :figsize, (6, 4)))
+    stacked = get(kwargs, :stacked, false)
 
     x,y,color = set_defaults(ax; kwargs...)
 
@@ -10,7 +11,8 @@ function bar(;kwargs...)
     xcount = Int64[sum(x .== v) for v in xvalues]
     cvalues = unique(color)
     ind = 1:length(xvalues)
-    width = 1/maximum(xcount) * 0.8
+    maxBarOverlap = stacked ? 1 : maximum(xcount)
+    width = 1/maxBarOverlap * 0.8
     groupSpacing = 0.2
 
     # find which colors are present at each x value
@@ -18,26 +20,29 @@ function bar(;kwargs...)
     for i in 1:length(xvalues)
         push!(cgroups, color[find(x .== xvalues[i])])
     end
-    
+
     bars = Any[]
-    for (i,c) in enumerate(cvalues)
-        vals = ones(length(xvalues))*Inf
+    lastVals = zeros(length(xvalues))
+    for (i,c) in (stacked ? reverse(collect(enumerate(cvalues))) : enumerate(cvalues))
         vals = Float64[]
         pos = Float64[]
         for j in 1:length(xvalues)
-            cind = findfirst(cgroups[j] .== c)
+            cind = stacked ? 1 : findfirst(cgroups[j] .== c)
             if cind != 0
                 push!(pos, j-1 + (cind-1)*width + width*0.1 + groupSpacing)
                 push!(vals, y[findfirst((color .== c) .* (x .== xvalues[j]))])
             end
         end
-        push!(bars, ax[:bar](pos, vals, width*0.8, color=colors[i], edgecolor="none"))
+        push!(bars, ax[:bar](pos, vals, width*0.8, color=colors[i], bottom=lastVals, edgecolor="none"))
+        if stacked
+            lastVals = vals
+        end
     end
     ax[:set_xlim](0,length(xvalues)+groupSpacing)
-    ax[:set_xticks](ind - 1 .+ width*maximum(xcount)/2 + groupSpacing)
+    ax[:set_xticks](ind - 1 .+ width*maxBarOverlap/2 + groupSpacing)
     ax[:set_xticklabels](xvalues)
 
     loc = get(kwargs, :legend, "upper right")
-    loc != "none" && ax[:legend](bars, cvalues, frameon=false, loc=loc)
+    loc != "none" && ax[:legend](stacked ? reverse(bars) : bars, cvalues, frameon=false, loc=loc)
     fig
 end
