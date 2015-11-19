ae12afec995f4be6d681a3cb226bfc731a4a828b
Scott Lundberg <scottmlundberg@gmail.com>
2015-11-19 14:36:14 -0800

    Allow xlabels to be disabled

--- src/bar.jl
+++ src/bar.jl
@@ -1,7 +1,7 @@
 
 function bar(;kwargs...)
     kwargs = Dict(kwargs)
-    fig,ax = subplots(figsize=get(kwargs, :figsize, (6, 4)))
+    fig,ax = subplots(figsize=get(kwargs, :figsize, (5, 3)))
     stacked = get(kwargs, :stacked, false)
 
     x,y,color = set_defaults(ax; kwargs...)
@@ -26,21 +26,24 @@ function bar(;kwargs...)
     for (i,c) in (stacked ? reverse(collect(enumerate(cvalues))) : enumerate(cvalues))
         vals = Float64[]
         pos = Float64[]
+        inds = Int64[]
         for j in 1:length(xvalues)
             cind = stacked ? 1 : findfirst(cgroups[j] .== c)
             if cind != 0
                 push!(pos, j-1 + (cind-1)*width + width*0.1 + groupSpacing)
                 push!(vals, y[findfirst((color .== c) .* (x .== xvalues[j]))])
+                push!(inds, j)
             end
         end
-        push!(bars, ax[:bar](pos, vals, width*0.8, color=colors[i], bottom=lastVals, edgecolor="none"))
+        push!(bars, ax[:bar](pos, vals, width*0.8, color=colors[i], bottom=lastVals[inds], edgecolor="none"))
         if stacked
-            lastVals = vals
+            lastVals[:] = 0
+            lastVals[inds] = vals
         end
     end
     ax[:set_xlim](0,length(xvalues)+groupSpacing)
     ax[:set_xticks](ind - 1 .+ width*maxBarOverlap/2 + groupSpacing)
-    ax[:set_xticklabels](xvalues)
+    ax[:set_xticklabels](get(kwargs, :xticklabels, true) ? xvalues : Any[])
 
     loc = get(kwargs, :legend, "upper right")
     loc != "none" && ax[:legend](stacked ? reverse(bars) : bars, cvalues, frameon=false, loc=loc)
