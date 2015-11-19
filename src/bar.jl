8c71b74785126f6d035931111bb6e3bdc808a562
Scott Lundberg <scottmlundberg@gmail.com>
2015-11-19 11:48:11 -0800

    Updated bar layout rules

--- src/bar.jl
+++ src/bar.jl
@@ -2,6 +2,7 @@
 function bar(;kwargs...)
     kwargs = Dict(kwargs)
     fig,ax = subplots(figsize=get(kwargs, :figsize, (6, 4)))
+
     x,y,color = set_defaults(ax; kwargs...)
 
     colors = get(kwargs, :colors, defaultColors)
@@ -17,7 +18,7 @@ function bar(;kwargs...)
     for i in 1:length(xvalues)
         push!(cgroups, color[find(x .== xvalues[i])])
     end
-
+    
     bars = Any[]
     for (i,c) in enumerate(cvalues)
         vals = ones(length(xvalues))*Inf
@@ -26,13 +27,14 @@ function bar(;kwargs...)
         for j in 1:length(xvalues)
             cind = findfirst(cgroups[j] .== c)
             if cind != 0
-                push!(pos, j + (cind-1)*width + groupSpacing)
+                push!(pos, j-1 + (cind-1)*width + width*0.1 + groupSpacing)
                 push!(vals, y[findfirst((color .== c) .* (x .== xvalues[j]))])
             end
         end
         push!(bars, ax[:bar](pos, vals, width*0.8, color=colors[i], edgecolor="none"))
     end
-    ax[:set_xticks](ind .+ width*maximum(xcount)/2 + groupSpacing - width*0.1)
+    ax[:set_xlim](0,length(xvalues)+groupSpacing)
+    ax[:set_xticks](ind - 1 .+ width*maximum(xcount)/2 + groupSpacing)
     ax[:set_xticklabels](xvalues)
 
     loc = get(kwargs, :legend, "upper right")
