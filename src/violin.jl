8aa54d0104412598cdd28b1e1cd3d7b4c66a48bc
Scott Lundberg <scottmlundberg@gmail.com>
2016-12-11 13:35:32 +0100

    Fixed the violin plot

--- src/violin.jl
+++ src/violin.jl
@@ -2,11 +2,20 @@
 export violin
 
 type ViolinLayer <: Layer
-    x::AbstractVector
-    data::AbstractVector
-    label
-    color
+    params::Dict{Any,Any}
+    unusedParams::Dict{Any,Any}
 end
+
+param(l::ViolinLayer, symbol) = get(l.params, symbol, get(violinDefaults, symbol, nothing))
+defaults(l::ViolinLayer) = violinDefaults
+supportslegend(l::ViolinLayer) = false
+violinDefaults = Dict(
+    :x => nothing,
+    :y => nothing,
+    :label => nothing,
+    :color => nothing,
+    :data => nothing
+)
 
 "Build a ViolinLayer"
 function violin(; kwargs...)
@@ -16,28 +25,22 @@ function violin(; kwargs...)
     @assert haskey(kwargs, :data) "data argument must be provided"
     @assert length(kwargs[:x]) == length(kwargs[:data]) "x and data arguments must be the same length"
 
-    ViolinLayer(
-        vec(kwargs[:x]),
-        kwargs[:data],
-        get(kwargs, :label, nothing),
-        get(kwargs, :color, nothing)
-    )
+    ViolinLayer(get_params(violinDefaults, kwargs)...)
 end
 violin(x, data; kwargs...) = violin(x=x, data=data; kwargs...)
 violin(x, data, label; kwargs...) = violin(x=x, data=data, label=label; kwargs...)
 
 "Process all layers on an axis to determine the violin layout settings."
-function violin_axis_parser(ax, state, layers...; kwargs...)
-    kwargs = Dict{Any,Any}(kwargs)
+function violin_axis_parser(ax, state, axis)
 
     # check for how many violin layers are present
-    violinLayers = collect(filter(l->typeof(l)==ViolinLayer, layers))
+    violinLayers = collect(filter(l->typeof(l)==ViolinLayer, param(axis, :layers)))
     if length(violinLayers) == 0 return end
-    allBar = length(violinLayers) == length(layers)
+    allBar = length(violinLayers) == length(param(axis, :layers))
 
-    x = vcat([l.x for l in violinLayers]...)
-    data = vcat([l.data for l in violinLayers]...)
-    labels = vcat([[l.label for x in l.x] for l in violinLayers]...)
+    x = vcat([param(l, :x) for l in violinLayers]...)
+    data = vcat([param(l, :data) for l in violinLayers]...)
+    labels = vcat([[param(l, :label) for x in param(l, :x)] for l in violinLayers]...)
     xvalues = unique(x)
     xcount = Int64[sum(x .== v) for v in xvalues]
     ind = 1:length(xvalues)
@@ -54,10 +57,9 @@ function violin_axis_parser(ax, state, layers...; kwargs...)
     ax[:set_xlim](0,length(xvalues)+groupSpacing)
     ax[:set_xticks](ind - 1 .+ width*maxXOverlap/2 + groupSpacing)
     ax[:xaxis][:set_ticks_position]("none")
-    ax[:set_xticklabels](get(kwargs, :xticklabels, true) ? xvalues : Any[])
     ax[:set_xticklabels](
-        get(kwargs, :xticklabels, true) ? xvalues : Any[],
-        rotation=get(kwargs, :xtickrotation, "horizontal")
+        param(axis, :xticklabels) != nothing ? param(axis, :xticklabels) : xvalues,
+        rotation=param(axis, :xtickrotation)
     )
 
     state[:violin_states] = Any[]
@@ -67,11 +69,11 @@ function violin_axis_parser(ax, state, layers...; kwargs...)
         inds = Int64[]
         widths = Float64[]
         for j in 1:length(xvalues)
-            cind = findfirst(cgroups[j] .== l.label)
+            cind = findfirst(cgroups[j] .== param(l, :label))
             if cind != 0
                 localWidth = (1/length(cgroups[j])) * 0.8
                 push!(pos, j-1 + (cind-1)*localWidth  + groupSpacing + localWidth/2)
-                push!(vals, data[findfirst((labels .== l.label) .* (x .== xvalues[j]))])
+                push!(vals, data[findfirst((labels .== param(l, :label)) .* (x .== xvalues[j]))])
                 push!(inds, j)
                 push!(widths, localWidth)
             end
@@ -83,7 +85,7 @@ function violin_axis_parser(ax, state, layers...; kwargs...)
     reverse!(state[:violin_states])
     state[:violin_ind] = 1 # used for counting which bar state to use when drawing
 end
-register_axis_parser(violin_axis_parser);
+register_axis_parser(violin_axis_parser)
 
 "Draw onto an axis"
 function draw(ax, state, l::ViolinLayer)
@@ -91,7 +93,7 @@ function draw(ax, state, l::ViolinLayer)
     s = state[:violin_states][i]
     p = ax[:violinplot](s[2], positions=s[1], widths=s[3], showextrema=false)
     for pc in p["bodies"]
-        pc[:set_facecolor](l.color)
+        pc[:set_facecolor](param(l, :color))
         pc[:set_edgecolor]("none")
         pc[:set_alpha](1)
     end
